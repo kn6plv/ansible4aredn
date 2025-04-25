d196a42eb9e922a76a6d27d6fea4922e7d4e6740
Tim Wilkinson <tim.j.wilkinson@gmail.com>
2025-04-25 09:58:20 -0700

    Update some commands to use ucode rather than lua

--- build.sh
+++ build.sh
@@ -1,5 +1,5 @@
 #! /bin/sh
 rm -f kn6plv-aredn-0.0.*.tar.gz
 ansible-galaxy collection build collections/ansible_collections/kn6plv/aredn
-#ansible-galaxy collection publish --token $(cat TOKEN) kn6plv-aredn-0.0.*.tar.gz
-#rm -f kn6plv-aredn-0.0.*.tar.gz
+ansible-galaxy collection publish kn6plv-aredn-0.0.*.tar.gz
+rm -f kn6plv-aredn-0.0.*.tar.gz
